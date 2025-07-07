9029c33ff402e8294944263a3b085c3fdfe8ee8e
Mayur <mayur@Mayurs-MacBook-Air.local>
2025-07-07 15:09:04 +0530

    my third coomit

--- pune-github.py
+++ pune-github.py
@@ -16,12 +16,4 @@ def save_tasks(tasks):
     with open(TODO_FILE, "w") as file:
         for task in tasks:
             file.write(task + "\n")
-# Display menu
-def show_menu():
-    print("\n===== To-Do List App =====")
-    print("1. View tasks")
-    print("2. Add task")
-    print("3. Remove task")
-    print("4. Mark task as completed")
-    print("5. Search task")
-    print("6. Exit")
+
